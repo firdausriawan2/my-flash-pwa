5b96671393d0f6e12d1d6265050dbc9e83d8eea7
M.Firdaus Riawan <firdausriawan2@gmail.com>
2024-06-21 10:11:03 +0700

    modified:   public/serviceworker.js

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -3,14 +3,14 @@ var filesToCache = [
     '/offline',
     // '/css/app.css',
     // '/js/app.js',
-    '/images/icons/icon-72x72.png',
-    '/images/icons/icon-96x96.png',
-    '/images/icons/icon-128x128.png',
-    '/images/icons/icon-144x144.png',
-    '/images/icons/icon-152x152.png',
-    '/images/icons/icon-192x192.png',
-    '/images/icons/icon-384x384.png',
-    '/images/icons/icon-512x512.png',
+    'assets/img/icon/72x72.png',
+    'assets/img/icon/96x96.png',
+    'assets/img/icon/128x128.png',
+    'assets/img/icon/144x144.png',
+    'assets/img/icon/152x152.png',
+    'assets/img/icon/192x192.png',
+    'assets/img/icon/384x384.png',
+    'assets/img/icon/512x512.png',
 ];
 
 // Cache on install
